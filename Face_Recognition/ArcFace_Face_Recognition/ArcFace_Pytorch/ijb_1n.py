00014eb34811cabb7afd3545c3ab74bdc6eeb59a
Martin Winter <martin.winter@joanneum.at>
2022-03-23 13:50:17 +0100

    preliminary version of modified script (1:N verification) for IJBx (do not use now)

---
+++ Face_Recognition/ArcFace_Face_Recognition/ArcFace_Pytorch/ijb_1n.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Note: This is modified content of the "ijb_1n.py" file downloaded from the
+# "insight-face" project site (GIT) located at
+# https://github.com/deepinsight/insightface/tree/master/recognition/_evaluation_/ijb (version 23.03.2022)
+# for usage within the XAIface project.
+
+import os
+import numpy as np
+import timeit
+import sklearn
+import cv2
+import sys
+import argparse
+import glob
+import numpy.matlib
+import heapq
+import math
+from datetime import datetime as dt
+import pandas as pd  # Note: pandas is imported for speeded up reading annotation-files
+
+from sklearn import preprocessing
+sys.path.append('./recognition')
+from eval_ijbc import Embedding  # Note: Embedding-class is imported from eval_ijbc, as the code is equivalent (checked)
+from menpo.visualize import print_progress
+from menpo.visualize.viewmatplotlib import sample_colours_from_colourmap
+
+
+def read_template_subject_id_list(path):
+    ijb_meta = np.loadtxt(path, dtype=str, skiprows=1, delimiter=',')
+    templates = ijb_meta[:, 0].astype(np.int)
+    subject_ids = ijb_meta[:, 1].astype(np.int)
+    return templates, subject_ids
+
+
+def read_template_media_list(path):
+    # ijb_meta = np.loadtxt(path, dtype=str)
+    ijb_meta = pd.read_csv(path, sep=' ', header=None).values  # Note: this is code is faster (using pandas)
+    templates = ijb_meta[:, 1].astype(np.int)
+    medias = ijb_meta[:, 2].astype(np.int)
+    return templates, medias
+
+
+def read_template_pair_list(path):
+    pairs = np.loadtxt(path, dtype=str)
+    t1 = pairs[:, 0].astype(np.int)
+    t2 = pairs[:, 1].astype(np.int)
+    label = pairs[:, 2].astype(np.int)
+    return t1, t2, label
+
+
+def image2template_feature(img_feats=None,  # used
+                           templates=None,
+                           medias=None,
+                           choose_templates=None,
+                           choose_ids=None):
+    # ==========================================================
+    # 1. face image feature l2 normalization. img_feats:[number_image x feats_dim]
+    # 2. compute media feature.
+    # 3. compute template feature.
+    # ==========================================================
+    unique_templates, indices = np.unique(choose_templates, return_index=True)
+    unique_subjectids = choose_ids[indices]
+    template_feats = np.zeros((len(unique_templates), img_feats.shape[1]))
+
+    for count_template, uqt in enumerate(unique_templates):
+        (ind_t, ) = np.where(templates == uqt)
+        face_norm_feats = img_feats[ind_t]
+        face_medias = medias[ind_t]
+        unique_medias, unique_media_counts = np.unique(face_medias,
+                                                       return_counts=True)
+        media_norm_feats = []
+        for u, ct in zip(unique_medias, unique_media_counts):
+            (ind_m, ) = np.where(face_medias == u)
+            if ct == 1:
+                media_norm_feats += [face_norm_feats[ind_m]]
+            else:  # image features from the same video will be aggregated into one feature
+                media_norm_feats += [
+                    np.mean(face_norm_feats[ind_m], 0, keepdims=True)
+                ]
+        media_norm_feats = np.array(media_norm_feats)
+        # media_norm_feats = media_norm_feats / np.sqrt(np.sum(media_norm_feats ** 2, -1, keepdims=True))
+        template_feats[count_template] = np.sum(media_norm_feats, 0)
+        if count_template % 2000 == 0:
+            print('\tfinished calculating {} template features.'.format(
+                count_template))
+    print('\tfinished calculating {} template features.'.format(count_template))
+    template_norm_feats = template_feats / np.sqrt(
+        np.sum(template_feats**2, -1, keepdims=True))
+    return template_norm_feats, unique_templates, unique_subjectids
+
+
+def verification(template_norm_feats=None,
+                 unique_templates=None,
+                 p1=None,
+                 p2=None):
+    # ==========================================================
+    #         Compute set-to-set Similarity Score.
+    # ==========================================================
+    template2id = np.zeros((max(unique_templates) + 1, 1), dtype=int)
+    for count_template, uqt in enumerate(unique_templates):
+        template2id[uqt] = count_template
+
+    score = np.zeros((len(p1), ))  # save cosine distance between pairs
+
+    total_pairs = np.array(range(len(p1)))
+    batchsize = 100000  # small batchsize instead of all pairs in one batch due to the memory limiation
+    sublists = [
+        total_pairs[i:i + batchsize] for i in range(0, len(p1), batchsize)
+    ]
+    total_sublists = len(sublists)
+    for c, s in enumerate(sublists):
+        feat1 = template_norm_feats[template2id[p1[s]]]
+        feat2 = template_norm_feats[template2id[p2[s]]]
+        similarity_score = np.sum(feat1 * feat2, -1)
+        score[s] = similarity_score.flatten()
+        if c % 10 == 0:
+            print('Finish {}/{} pairs.'.format(c, total_sublists))
+    return score
+
+
+def read_score(path):
+    with open(path, 'rb') as fid:
+        img_feats = cPickle.load(fid)
+    return img_feats
+
+
+def evaluation(query_feats, gallery_feats, mask):
+    Fars = [0.01, 0.1]
+    print(query_feats.shape)
+    print(gallery_feats.shape)
+
+    query_num = query_feats.shape[0]
+    gallery_num = gallery_feats.shape[0]
+
+    similarity = np.dot(query_feats, gallery_feats.T)
+    print('similarity shape', similarity.shape)
+    top_inds = np.argsort(-similarity)
+    print(top_inds.shape)
+
+    # calculate top1
+    correct_num = 0
+    for i in range(query_num):
+        j = top_inds[i, 0]
+        if j == mask[i]:
+            correct_num += 1
+    print("top1 = {}".format(correct_num / query_num))
+    # calculate top5
+    correct_num = 0
+    for i in range(query_num):
+        j = top_inds[i, 0:5]
+        if mask[i] in j:
+            correct_num += 1
+    print("top5 = {}".format(correct_num / query_num))
+    # calculate 10
+    correct_num = 0
+    for i in range(query_num):
+        j = top_inds[i, 0:10]
+        if mask[i] in j:
+            correct_num += 1
+    print("top10 = {}".format(correct_num / query_num))
+
+    neg_pair_num = query_num * gallery_num - query_num
+    print(neg_pair_num)
+    required_topk = [math.ceil(query_num * x) for x in Fars]
+    top_sims = similarity
+    # calculate fars and tprs
+    pos_sims = []
+    for i in range(query_num):
+        gt = mask[i]
+        pos_sims.append(top_sims[i, gt])
+        top_sims[i, gt] = -2.0
+
+    pos_sims = np.array(pos_sims)
+    print(pos_sims.shape)
+    neg_sims = top_sims[np.where(top_sims > -2.0)]
+    print("neg_sims num = {}".format(len(neg_sims)))
+    neg_sims = heapq.nlargest(max(required_topk), neg_sims)  # heap sort
+    print("after sorting , neg_sims num = {}".format(len(neg_sims)))
+    for far, pos in zip(Fars, required_topk):
+        th = neg_sims[pos - 1]
+        recall = np.sum(pos_sims > th) / query_num
+        print("far = {:.10f} pr = {:.10f} th = {:.10f}".format(
+            far, recall, th))
+
+
+def gen_mask(query_ids, reg_ids):  # used
+    mask = []
+    for query_id in query_ids:
+        pos = [i for i, x in enumerate(reg_ids) if query_id == x]
+        if len(pos) != 1:
+            raise RuntimeError(
+                "RegIdsError with id = {}， duplicate = {} ".format(
+                    query_id, len(pos)))
+        mask.append(pos[0])
+    return mask
+
+
+if __name__ == "__main__":
+    pass
